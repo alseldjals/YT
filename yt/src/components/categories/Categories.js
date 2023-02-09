61b5a8f546e20589573b9c86685e31d3de35333e
alseldjals <alseldjals@naver.com>
2023-02-09 22:27:00 +0900

    카테고리 바

--- yt/src/components/categories/Categories.js
+++ yt/src/components/categories/Categories.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Categories.scss';
 
+const keywords = [
+    '전체',
+    '먹방',
+    '일상',
+    '게임',
+    '엔터테인먼트',
+    '음악',
+    'ASMR',
+    '요리',
+    '스포츠',
+    '경제',
+    '뉴스',
+]
 const Categories = () => {
+
+    const [activeElement, setActiveElement] = useState('All')
+
+    const handleClick = value =>{
+        setActiveElement(value)
+    }
+
     return (
-        <div>
-            Categories
+        <div className='categories'>
+           {keywords.map((value, i) => (
+              <span
+                 onClick={() => handleClick(value)}
+                 key={i}
+                 className={activeElement === value ? 'active' : ''}>
+                 {value}
+              </span>
+           ))}
         </div>
-    )
-}
+     )
+  }
+  
 
 export default Categories;
