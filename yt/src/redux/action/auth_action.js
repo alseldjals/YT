d5c0718cf5f2c84d737f205ce5432b56ed9d8547
alseldjals <alseldjals@naver.com>
2023-02-12 22:46:41 +0900

    구글 로그인 연동

--- yt/src/redux/action/auth_action.js
+++ yt/src/redux/action/auth_action.js
@@ -1,11 +1,37 @@
 import firebase from 'firebase/compat/app';
 import auth from '../../firebase';
+import { LOAD_PROFILE, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS } from '../actiontype';
 
 export const login = () => async dispatch => {
     try {
+
+        dispatch({
+            type: LOGIN_REQUEST,
+        })
         const provider = new firebase.auth.GoogleAuthProvider()
 
         const res = await auth.signInWithPopup(provider)
         console.lor(res)
-    } catch(error) {}
+
+        const accessToken = res.credential.accessToken
+
+        const profile = {
+            name: res.additionalUserInfo.profile.displayName,
+            pthotoURL:res.additionalUserInfo.profile.photoURL,
+        }
+        dispatch ({
+            type: LOGIN_SUCCESS,
+            payload: accessToken
+        })
+        dispatch({
+            type:LOAD_PROFILE,
+            payload: profile,
+        })
+    } catch(error) {
+        console.log(error.message);
+        dispatch({
+            type:LOGIN_FAIL,
+            payload:error.message,
+        })
+    }
 }
